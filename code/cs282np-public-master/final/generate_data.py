a6ff1b1d52041621c01bccf4fe7e9c092ef87dfd
Morris Yau <morrisyau@Morriss-MacBook-Air.local>
2017-04-17 00:26:14 -0400

    riddled with off by ones in pb sample

--- code/cs282np-public-master/final/generate_data.py
+++ code/cs282np-public-master/final/generate_data.py
@@ -16,19 +16,71 @@ print("Hello")
 
 
 #size of paintbox
-f = 2 #features
-d = 2 #discretization
+F = 2 #features
+D = 2**F #discretization
+T = F #length of datapoint
 
 #data size 
-
-#paintbox 
-pb = np.zeros(f,d)
+N = 10
 
 #initalize features
-W = np.eye(f)
+#this is a hack, W is F by T matrix 
+W = np.eye(F)
 
 #noise parameter
 sig = 0.1
 
 #generate data
+#set paintbox to be anything
+
+def pb_partition(D,F):
+    pb = np.zeros((F,D))
+    for i in range(F):
+        print(D/F)
+        try:
+            pb[i,i*D/F:(i+1)*D/F] = [1]*(D/F)
+        except ValueError: 
+            print("Value Error")
+    return pb
+    
+def pb_init(D,F):
+    pb = np.zeros((F,D))
+    for i in range(F):
+        pb[i,:] = np.tile([1]*(D/(2**(i+1))) + [0]*(D/(2**(i+1))),2**i)
+    return pb
+    
+def draw_Z(pb,D,F,N,T):
+    draws = np.random.multinomial(N, [1.0/D]*D, size=1)
+    print(draws)
+    Z = np.zeros((N,F))
+    cum_draws = np.cumsum(draws)
+    #generate Z
+    for i in range(D):
+        density = draws[0,i]
+        row = pb[:,i]
+        data_chunk = np.tile(row,(density,1))
+        if i == 0:
+             Z[0:cum_draws[i],:] = data_chunk
+        else:    
+            Z[cum_draws[i-1]:cum_draws[i],:] = data_chunk
+    return Z
+
+def generate_data(D,F,N,T,sig):
+    pb = pb_partition(D,F)
+    print(pb)
+    #This line is problematic, does not adapt to T
+    W = np.eye(F)
+    Y = np.zeros((N,T))
+    E = np.reshape(np.random.normal(0,sig,N*T),(N,T))
+    
+    Z = draw_Z(pb,D,F,N,T)
+    #consider ignoring the noise
+    #Y = np.dot(Z,W) + E
+    Y = np.dot(Z,W) + E
+    return Y    
+    
+Y = generate_data(D,F,N,T,sig)
+print(Y)
+
+
 
