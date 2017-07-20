388afa6541431f96c354d5edc77e8d2439875a2a
Morris Yau <morrisyau@Morriss-MacBook-Air.local>
2017-07-20 18:31:32 +0800

    submit hubble again

--- code/cs282np-public-master/final/jobs.py
+++ code/cs282np-public-master/final/jobs.py
@@ -5,20 +5,22 @@ args = sys.argv
 print(args)
 number_runs = args[1]
 alg_type = ['paintbox']
-param = [3]
+param = [.006,.009,.012,.015]
+held = [.7,.8,.9]
 for alg in alg_type:
     for par in param:
-        fo = open("run" + alg + str(par) + ".sh", "w")
-        fo.write("#!/bin/bash\n")
-        fo.write("#BATCH -J single_fouriernetwork\n")
-        fo.write("#SBATCH -o pl" + alg + str(par) + "_%a.out\n")
-        fo.write("#SBATCH -e pl" + alg +str(par)+ "_%a.err\n")
-        fo.write("#SBATCH -N 1\n")
-        fo.write("#SBATCH -n 32\n")
-        fo.write("#SBATCH -t 0-40:00\n")
-        fo.write("#SBATCH -p serial_requeue\n")
-        fo.write("#SBATCH --mem=32000\n")
-        fo.write("module load python\n")
-        fo.write("python -W ignore real_tests.py " + alg + " " + str(par) + "\n")
-        fo.close()
-        os.system("sbatch --array=1-" + number_runs + " run" + alg + str(par) + ".sh")
+        for he in held:
+            fo = open("run" + alg + str(par) + str(he) + ".sh", "w")
+            fo.write("#!/bin/bash\n")
+            fo.write("#BATCH -J single_fouriernetwork\n")
+            fo.write("#SBATCH -o ko" + alg + str(par) + str(he) + "_%a.out\n")
+            fo.write("#SBATCH -e ko" + alg +str(par)+ str(he) + "_%a.err\n")
+            fo.write("#SBATCH -N 1\n")
+            fo.write("#SBATCH -n 32\n")
+            fo.write("#SBATCH -t 0-40:00\n")
+            fo.write("#SBATCH -p doshi-velez\n")
+            fo.write("#SBATCH --mem=32000\n")
+            fo.write("module load python\n")
+            fo.write("python -W ignore d2_tests.py " + alg + " " + str(par) + " " + str(he) + "\n")
+            fo.close()
+            os.system("sbatch --array=1-" + number_runs + " run" + alg + str(par) + str(he) + ".sh")
